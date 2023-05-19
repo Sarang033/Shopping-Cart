fa3575fa77963602e060baa19b3a7e685189c956
Sarang Sharma <sarangchamp2--4>
2023-05-19 14:11:44 +0530

    fixed card issue

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,46 @@
+import { Button, Container, Nav, Navbar as NavbarBS } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+export function Navbar() {
+  return (
+    <NavbarBS sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        <Button
+          style={{ width: "3rem", height: "3rem", position: "relative" }}
+          variant="outline-primary"
+          className="rounded-circle"
+        >
+          <img
+            style={{ width: "1.5rem", height: "2rem" }}
+            src="https://www.svgrepo.com/show/80543/shopping-cart-outline.svg"
+          />
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center "
+            style={{
+              color: "white",
+              width: "1.5rem",
+              height: "1.5rem",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              transform: "translate(25%,25%)",
+            }}
+          >
+            3
+          </div>
+        </Button>
+      </Container>
+    </NavbarBS>
+  );
+}
